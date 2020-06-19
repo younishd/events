0931674841ebc270c2b263e76559f6459336d722
YouniS <younis.bensalah@gmail.com>
2020-06-19 18:51:58 +0200

    Add getters and setters to ScheduleElement

--- src/events/ScheduleElement.php
+++ src/events/ScheduleElement.php
@@ -9,21 +9,41 @@ class ScheduleElement
 
     private $event;
 
-    private $temp_expr;
+    private $expression;
 
     public function __construct(
             Event $e,
             TemporalExpression $t)
     {
         $this->event = $e;
-        $this->temp_expr = $t;
+        $this->expression = $t;
     }
 
     public function isOccurring(
             Event $e,
-            \DateTimeInterface $d) : bool
+            \DateTimeInterface $d): bool
     {
-        return $this->event == $e && $this->temp_expr->includes($d);
+        return $this->event == $e && $this->expression->includes($d);
+    }
+
+    public function getEvent(): Event
+    {
+        //
+    }
+
+    public function setEvent(Event $e)
+    {
+        //
+    }
+
+    public function getTemporalExpression(): TemporalExpression
+    {
+        //
+    }
+
+    public function setTemporalExpression(TemporalExpression $t)
+    {
+        //
     }
 
 }
